c257f8f7bbbcfef80b8aff7558fbb6d0a0536bd4
belenkanas <maria.kanas@correo.ucu.edu.uy>
2024-11-14 01:31:38 -0300

    Items copiados

---
+++ src/Library/TiposItem/Revivir.cs
@@ -0,0 +1,46 @@
+namespace Library
+{
+    /// <summary>
+    /// Este tipo de Item logra que se reviva a un Pokemon con el 50% de su HP total.
+    /// </summary>
+    public class Revivir : IItem
+    {
+        public string NombreItem { get; }
+        private int usosRestantes;
+        private const int usosMaximos = 1;
+
+        public Revivir()
+        {
+            NombreItem = "Revivir";
+            usosRestantes = usosMaximos; 
+        }
+
+        /// <summary>
+        /// Metodo para quitar los efectos
+        /// </summary>
+        /// <param name="VidaActual"></param>
+        /// <param name="VidaTotal"></param>
+        /// <returns></returns>
+        public double Usar(double VidaActual, double VidaTotal)
+        {
+            if (usosRestantes > 0)
+            {
+                if (VidaActual == 0) // Compara si la vida es 0
+                {
+                    usosRestantes--; 
+                    return VidaActual = VidaTotal * 0.5;
+                }
+                else
+                {
+                    Console.WriteLine("Solo se puede revivir cuando el pokemon está muerto");
+                    return VidaActual; // Devuelve la vida actual sin cambios
+                }
+            }
+            else
+            {
+                Console.WriteLine("No se pueden usar más 'Revivir' en esta batalla.");
+                return VidaActual;
+            }
+        }
+    }
+}
---
+++ src/Library/TiposItem/SuperPocion.cs
@@ -0,0 +1,45 @@
+namespace Library;
+
+/// <summary>
+/// Con este tipo de Item, se pueden usar hasta 4 y cada una recupera 70 puntos de HP.
+/// </summary>
+public class SuperPocion: IItem
+{
+    public string NombreItem { get; }
+    private int usosRestantes;
+    private const int usosMaximos = 4;
+
+    public SuperPocion()
+    {
+        NombreItem = "Super Pocion";
+        usosRestantes = usosMaximos; // Inicializa el contador de usos
+    }
+
+    /// <summary>
+    /// Método para curar, recibe la vida actual y la total del pokemón e intenta reestablecerla.
+    /// </summary>
+    /// <param name="VidaActual"></param>
+    /// <param name="VidaTotal"></param>
+    /// <returns></returns>
+    public double Usar(double VidaActual, double VidaTotal)
+    {
+        if (usosRestantes > 0)
+        {
+            if (VidaActual < VidaTotal - 70)
+            {
+                usosRestantes--; // Reduce el contador de usos
+                return VidaActual + 70; // Si la diferencia es mayor a 70, se curan 70 puntos
+            }
+            else
+            {
+                usosRestantes--; // Reduce el contador de usos
+                return VidaTotal; // Si no, se cura hasta el máximo (VidaTotal)
+            }
+        }
+        else
+        {
+            Console.WriteLine("No se pueden usar más 'Super Pocion' en esta batalla.");
+            return VidaActual; // Devuelve la vida actual sin cambios
+        }
+    }
+}
