4a1f94c2be4a8e599ad8ca878b5ce476cea7258a
belenkanas <maria.kanas@correo.ucu.edu.uy>
2024-11-20 12:02:56 -0300

    Mostrar vida comando

---
+++ src/Library/Commands/PokemonCommands/MostrarVidaCommand.cs
@@ -0,0 +1,30 @@
+﻿using Discord.Commands;
+using Library;
+using Ucu.Poo.DiscordBot.Domain;
+
+namespace Ucu.Poo.DiscordBot.Commands;
+
+/// <summary>
+/// Esta clase implementa el comando 'mostrarvida' del bot. Este comando muestra
+/// los niveles HP del pokemon del equipo (indicado por un índice).
+/// </summary>
+public class MostrarVidaCommand : ModuleBase<SocketCommandContext>
+{
+    private static Dictionary<string, JugadorPrincipal> jugadores = new Dictionary<string, JugadorPrincipal>();
+
+    /// <summary>
+    /// Implementa el comando 'mostrarvida'. Este comando muestra la lista de
+    /// items disponibles para usar en la batalla.
+    /// </summary>
+    [Command("mostrarvida")]
+    [Summary("Muestra la vida del pokemon que está en batalla, elegido por el índice")]
+    public async Task ExecuteAsync(int indice)
+    {
+        string displayName = Context.User.Username;
+        JugadorPrincipal jugadorPrincipal = jugadores[displayName];
+        IPokemon pokemon = jugadorPrincipal.ElegirPokemon(indice);
+        string resultado = pokemon.MostrarVida();
+        
+        await ReplyAsync(resultado);
+    }
+}
---
+++ src/Library/Commands/ShowTurnCommand.cs
@@ -0,0 +1,29 @@
+﻿using Discord.Commands;
+using Library;
+
+namespace Ucu.Poo.DiscordBot.Commands;
+/// <summary>
+/// Esta clase implementa el comando 'mostrarturno' del bot. Este comando le
+/// permite al entrenador ver si es su turno o no.
+/// </summary>
+public class ShowTurnCommand : ModuleBase<SocketCommandContext>
+{
+    private static Dictionary<string, BatallaFacade> batallasEnCurso;
+
+    /// <summary>
+    /// Implementa el comando 'mostrarturno'. Este comando permite que el entrenador
+    /// vea si es su turno o no.
+    /// </summary>
+    [Command("mostrarturno")]
+    [Summary("Permite que el jugador vea si es su turno o no")]
+    public async Task ExecuteAsync(int indiceAtaque)
+    {
+        string displayName = CommandHelper.GetDisplayName(Context);
+        if (batallasEnCurso.TryGetValue(displayName, out BatallaFacade batalla))
+        {
+            string resultado = batalla.VerificarTurno(displayName).ToString();
+            await ReplyAsync(resultado);
+
+        }
+    }
+}
