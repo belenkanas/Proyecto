9bd24f03ff8b7afc725b5c89c9cd78d87279a172
belenkanas <maria.kanas@correo.ucu.edu.uy>
2024-11-25 10:17:26 -0300

    servidor belen

---
+++ test/LibraryTests/ItemsCommandTest.cs
@@ -0,0 +1,42 @@
+﻿using Library;
+using NUnit.Framework;
+using Ucu.Poo.DiscordBot.Commands;
+using Ucu.Poo.DiscordBot.Services;
+
+
+namespace LibraryTests;
+
+public class ItemsCommandTest
+{
+    public JugadorPrincipal jugador;
+    private static Dictionary<string, JugadorPrincipal> jugadores = new Dictionary<string, JugadorPrincipal>();
+
+    [SetUp]
+    public void SetUp()
+    {
+        void DemoBot()
+        {
+            BotLoader.LoadAsync().GetAwaiter().GetResult();
+        }
+        DemoBot();
+        jugador = new JugadorPrincipal("Sol");
+        jugadores.Add("Sol",jugador);
+    }
+    
+    [Test]
+    public async Task MostrarItemsTest()
+    {
+        string displayName = jugador.NombreJugador;
+
+        JugadorPrincipal jugadorPrincipal = jugadores[displayName];
+
+        string cadena = $"Inventario de Items:\n " +
+                        $"1. Super Pocion \n" +
+                        $"2. Revivir \n" +
+                        $"3. Cura Total \n";
+        
+        string inventario = jugadorPrincipal.MostrarInventario();
+        
+        Assert.That(cadena,Is.EqualTo(inventario));
+    }
+}
